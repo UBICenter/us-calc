f7193e07968cbf73b4e47fdd0c87aaf1052b1228
Will Fedder <fedderw@gmail.com>
2021-05-05 15:00:42 -0400

    add all_state_stats and demog_stats files to repo

--- pre-processing.py
+++ pre-processing.py
@@ -4,13 +4,6 @@
 
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 import microdf as mdf
 import os
 import us
@@ -58,6 +51,7 @@ person["state"] = (
     # change us package formatting to string
     .astype(str)
 )
+
 # drop original statefip column from dataframe
 person = person.drop(columns=["statefip"])
 
@@ -117,3 +111,104 @@ spmu.rename(columns={"person": "numper"}, inplace=True)
 # write pre-processed dfs to csv files
 person.to_csv("person.csv.gz", compression="gzip")
 spmu.to_csv("spmu.csv.gz", compression="gzip")
+
+# create boolean column for individual's poverty status, 1=poor
+person["poor"] = person.spmthresh > person.spmtotres
+
+# create a column for all selected demographic variables
+# that will be used to calculate poverty rates
+demog_cols = [
+    "person",
+    "adult",
+    "child",
+    "black",
+    "white_non_hispanic",
+    "hispanic",
+    "pwd",
+    "non_citizen",
+    "non_citizen_adult",
+    "non_citizen_child",
+]
+
+poor_pop = person.loc[person.poor]
+
+# calculate weighted sum of people living in poverty
+mdf.weighted_sum(poor_pop, demog_cols, "asecwt")
+# calculate poverty RATE for each DEMOGRAPHIC in US
+pov_rate_us = mdf.weighted_sum(poor_pop, demog_cols, "asecwt") / mdf.weighted_sum(
+    person, demog_cols, w="asecwt"
+)
+# add name to series
+pov_rate_us.name = "US"
+# calculate poverty RATE for each group by state
+pov_rates = mdf.weighted_sum(
+    poor_pop, demog_cols, "asecwt", groupby="state"
+) / mdf.weighted_sum(person, demog_cols, w="asecwt", groupby="state")
+
+# append US statistics as additional 'state'
+pov_df = pov_rates.append(pov_rate_us)
+
+# melt df from wide to long format
+pov_df = pov_df.melt(ignore_index=False, var_name="demog")
+# insert column indicating metric in question
+pov_df.insert(loc=1, column="metric", value="pov_rate")
+
+
+##
+# calculate POPULATION for each DEMOGRAPHIC in US
+pop_us = mdf.weighted_sum(person, demog_cols, w="asecwt")
+# add name to series
+pop_us.name = "US"
+# calculate POPULATION for each group by state
+pop_states = mdf.weighted_sum(person, demog_cols, w="asecwt", groupby="state")
+# append US statistics as additional 'state'
+pop_df = pop_states.append(pop_us)
+# melt df from wide to long format
+pop_df = pop_df.melt(ignore_index=False, var_name="demog")
+pop_df.insert(loc=1, column="metric", value="pop")
+
+# concat poverty and population dfs
+demog_stats = pd.concat([pov_df, pop_df])
+# write to csv file
+demog_stats.to_csv("demog_stats.csv.gz", compression="gzip")
+
+# Caluclate original gini
+person["spm_resources_per_person"] = person.spmtotres / person.numper
+# Caluclate original gini for US
+gini_us = pd.Series(mdf.gini(person, "spm_resources_per_person", "asecwt"))
+# add name to series
+gini_us.index = ["US"]
+
+
+# calculate gini for each group by state
+gini_states = mdf.gini(person, "spm_resources_per_person", w="asecwt", groupby="state")
+
+# append US statistics as additional 'state'
+gini_ser = gini_states.append(gini_us)
+gini_ser.name = "gini"
+
+# Calculate the original poverty gap
+spmu["poverty_gap"] = np.where(
+    spmu.spmtotres < spmu.spmthresh,
+    spmu.spmthresh - spmu.spmtotres,
+    0,
+)
+poverty_gap_us = pd.Series(mdf.weighted_sum(spmu, "poverty_gap", w="spmwt"))
+
+
+# add name to series
+poverty_gap_us.index = ["US"]
+
+
+# calculate gini for each group by state
+poverty_gap_states = mdf.weighted_sum(spmu, "poverty_gap", w="spmwt", groupby="state")
+
+# append US statistics as additional 'state'
+poverty_gap_ser = poverty_gap_states.append(poverty_gap_us)
+poverty_gap_ser.name = "poverty_gap"
+
+all_state_stats = poverty_gap_ser.to_frame().merge(
+    gini_ser.to_frame(), left_index=True, right_index=True
+)
+
+all_state_stats.to_csv("all_state_stats.csv.gz", compression="gzip")
