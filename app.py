4b6a8b76285f56f4ef763880f41f43d7d7a4c4d7
Will Fedder <fedderw@gmail.com>
2021-09-07 16:27:34 -0400

    fix logo url

--- app.py
+++ app.py
@@ -291,9 +291,10 @@ app.layout = html.Div(
                             dbc.Col(
                                 # insert logo
                                 html.Img(
-                                    src="https://blog.ubicenter.org/_static/ubi_center_logo_wide_blue.png",
+                                    # src="https://blog.ubicenter.org/_static/ubi_center_logo_wide_blue.png",
+                                    src="https://raw.githubusercontent.com/UBICenter/ubicenter.org/master/assets/images/logos/wide-blue.jpg",
                                     height="30px",
-                                )
+                                ),
                             ),
                         ],
                         align="center",
